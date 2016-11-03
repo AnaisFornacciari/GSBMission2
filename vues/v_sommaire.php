f16c79e5373f33e386a78f8db189dbc1e3fa3684
ANAIS <ANAIS@192.168.0.22>
2016-11-03 18:05:10 +0100

    initialisation

---
+++ vues/v_sommaire.php
@@ -0,0 +1,47 @@
+﻿    <!-- Division pour le sommaire -->
+<div id="menuGauche">
+    <div id="infosUtil">
+    </div>  
+    <?php 
+    if(is_array($visiteur))
+    {
+        ?>
+        <ul id="menuList">
+           <li>
+                <?php echo "Visiteur :<br>";
+                echo $_SESSION['prenom']."  ".$_SESSION['nom'] ?>
+            </li>
+            <li class="smenu">
+                <a href="saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+            </li>
+            <li class="smenu">
+                <a href="selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+            </li>
+            <li class="smenu">
+                <a href="deconnecter" title="Se déconnecter">Déconnexion</a>
+            </li>
+        </ul>
+        <?php
+    }
+    else 
+    {
+        ?>
+        <ul id="menuList">
+            <li>
+                <?php echo "Comptable :<br>";
+                echo $_SESSION['prenom']."  ".$_SESSION['nom'] ?>
+            </li>
+            <li class="smenu">
+                <a href="validerFiche" title="Valider fiche de frais">Valider fiche de frais</a>
+            </li>
+            <li class="smenu">
+                <a href="genererEtatQuotidien" title="Consultation des fiches de frais Valider">Générer mon état quotidien</a>
+            </li>
+            <li class="smenu">
+                <a href="deconnecter" title="Se déconnecter">Déconnexion</a>
+            </li>
+        </ul>
+        <?php
+    }
+    ?>
+ </div>
